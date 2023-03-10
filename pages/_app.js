f65cf4a9827a9ef973e3473aad267fa115689427
isitusnoamow <240135@hkis.edu.hk>
2023-03-10 22:48:34 +0800

    dev

--- pages/_app.js
+++ pages/_app.js
@@ -12,4 +12,5 @@ function MyApp({ Component, pageProps }) {
     </SessionContextProvider>
   )
 }
+
 export default MyApp
--- pages/login.js
+++ pages/login.js
@@ -1,13 +1,17 @@
 import { Auth, ThemeSupa } from '@supabase/auth-ui-react'
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
 import style from "../styles/login.module.css"
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs'
 
 export default function Login() {
   const session = useSession()
   const supabase = useSupabaseClient()
+  const router = useRouter()
 
   return (
-    <div className=''>
+    <div className={style['full']}>
       <div className={style["container"]} style={{ padding: '50px 0 100px 0' }}>
         {!session ? (
           <div className={style.contain}>
@@ -22,4 +26,26 @@ export default function Login() {
       </div>
     </div>
   )
+}
+
+export const getServerSideProps = async (ctx) => {
+  const supabase = createServerSupabaseClient(ctx)
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  if(session) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: {
+      initialSession: session,
+    }
+  }
 }
